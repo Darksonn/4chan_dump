5b5306979652b92f2664dff82c25e12317bae1fa
Darksonn <kristoffer@ryhl.dk>
2015-05-03 12:48:28 +0200

    Commited code

---
+++ find_images.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char expected[] = "i.4cdn.org/\1\2\3\0";
+
+int main(void) {
+  char c;
+  char url[512];
+  int r = 0, ei = 0, i;
+  while ((c = getchar()) != EOF) {
+    char e = expected[ei++];
+    //fprintf(stderr, "read: %c, expected %d, ei %d, r %d\n", c, (int) e, ei, r);
+    if (e == 0) {
+      url[r - 1] = 0;
+      printf("%s\n", url);
+      for (i = 0; i < r; i++)
+        url[i] = 0;
+      r = 0;
+      ei = 0;
+    } else if (e == 1) {
+      if (c != '/')
+        ei--;
+      url[r++] = c;
+    } else if (e == 2) {
+      if (c >= '0' && c <= '9')
+        ei--;
+      else if (c != '.')
+        goto restart;
+      url[r++] = c;
+    } else if (e == 3) {
+      char check = c;
+      if ('Z' >= check)
+        check ^= ' ';
+      if (check >= 'a' && check <= 'z')
+        ei--;
+      url[r++] = c;
+    } else if (e == c) {
+      url[r++] = c;
+    } else {
+restart:
+      for (i = 0; i < r; i++)
+        url[i] = 0;
+      r = 0;
+      ei = 0;
+    }
+  }
+  return 0;
+}
+
